c0f093011cf7629f7e109c30c04a3ed8883e3b4c
Dan Morrison <danielroy.morrison@gmail.com>
2015-03-03 15:46:59 +1300

    set up rails skeleton with signin function

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+
+    if !user_signed_in?
+      redirect '/users/sign_in'
+    end
+    
+  end
+
+end

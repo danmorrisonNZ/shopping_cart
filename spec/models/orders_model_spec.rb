41cedf9fb74523b086126a6f5d5de0f9ee391026
Dan Morrison <danielroy.morrison@gmail.com>
2015-03-04 12:21:19 +1300

    added test outlines for orders model

---
+++ spec/models/orders_model_spec.rb
@@ -0,0 +1,36 @@
+require 'rails helper'
+
+RSpec.describe Order, :type => :model do
+
+  describe 'total_price' do
+
+    it 'exists' do
+    end
+
+    it 'produces the price as a string' do
+    end
+
+    it 'is the sum of all prices associated with the order' do
+    end
+
+    it 'is rounded to two decimal places' do
+    end
+
+  end
+
+
+  describe 'item_count' do
+
+    it 'exists' do
+    end
+
+    it 'produces the count as a string' do
+    end
+
+    it 'is the sum of the number of items in an order' do
+    end
+    
+
+  end
+
+end

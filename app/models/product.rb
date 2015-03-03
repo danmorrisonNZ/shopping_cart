83ccb6aaac718c08a7a6a1e4c0c47716ae520d26
Dan Morrison <danielroy.morrison@gmail.com>
2015-03-04 11:56:50 +1300

    added tests for product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 class Product < ActiveRecord::Base
 
   def format_price
+    new_price = price.to_s
+    new_price << '0' if new_price.split('.').last.length < 2
+    new_price.insert(0,'$')
   end
 
 end
--- spec/models/product_model_spec.rb
+++ spec/models/product_model_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Product, :type => :model do
 
   let(:product) {Product.create}
 
-
   describe 'name' do
 
     it 'exists' do
@@ -12,6 +11,9 @@ RSpec.describe Product, :type => :model do
     end
 
     it 'is unique' do
+      beef_sausage = Product.create(name: "sausage")
+      chicken_sausage = Product.create(name: "sausage")
+      expect(chicken_sausage).to be_nil
     end
 
   end
@@ -20,9 +22,11 @@ RSpec.describe Product, :type => :model do
   describe 'price' do
 
     it 'exists' do
+      expect(product.price).to_not be_nil
     end
 
     it 'is an integer' do
+      expect(product.price).to be_an(Integer)
     end
 
   end
@@ -30,16 +34,24 @@ RSpec.describe Product, :type => :model do
 
   describe '#format_price' do
 
+    before do
+      @cereal = Product.create(name: "cereal", price: 5.00)
+    end
+
     it 'returns a string' do
+      expect(@cereal.format_price).to be_a(String)
     end
 
     it 'has two decimal places' do
+      expect(@cereal.format_price.split('.').last.length).to eq(2)
     end
 
     it 'begins with a dollar sign' do
+      expect(@cereal.format_price[0]).to eq("$")
     end
 
     it 'conatins the same value as the original price' do
+      expect(@cereal.format_price.delete('$').to_f).to eq(@cereal.price)
     end
 
   end
@@ -48,9 +60,11 @@ RSpec.describe Product, :type => :model do
   describe 'description' do
 
     it 'exists' do
+      expect(product.description).to_not be_nil
     end
 
     it 'is a string' do
+      expect(product.description).to be_a(String)
     end
 
   end

f88c3b08c864841bd397d79210699b22dcaed8ee
Dan Morrison <danielroy.morrison@gmail.com>
2015-03-05 10:49:25 +1300

    first test for products_controller failiong

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -2,6 +2,20 @@ require "rails_helper"
 
 RSpec.describe ProductsController, :type => :controller do
 
+ let(:item_double) { double("item_double")}
+
+   before do
+     allow(Product).to receive(:new).and_return(item_double)
+     get :new
+   end
+
+   it 'creates a new product' do
+     expect(assigns(:product)).to be(item_double)
+   end
+
+
+   
+
 
 
 end
